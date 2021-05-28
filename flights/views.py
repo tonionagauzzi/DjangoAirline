50b8d1f914e30bd2b11095c1cbf808ad84b0e2e0
nagauzzi <kensaku.nagauchi@access-company.com>
2021-05-28 09:31:26 +0900

    Flight View

--- flights/views.py
+++ flights/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
+from .models import Flight, Airport
 
 def index(request):
-    return HttpResponse("Hello, world!")
+    return render(request, "flights/index.html", {
+        "flights": Flight.objects.all()
+    })
+
+def flight(request, flight_id):
+    flight = Flight.objects.get(id=flight_id)
+    return render(request, "flights/flight.html", {
+        "flight": flight
+    })
